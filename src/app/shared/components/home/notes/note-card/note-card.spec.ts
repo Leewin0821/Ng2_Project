e93b66a80ae638996ae5270c7a8b401b70d2aeaf
lwzhang <lwzhang@thoughtworks.com>
2016-10-20 00:22:46 +0800

    [Leewin] Added ut for note card

--- src/app/shared/components/home/notes/note-card/note-card.spec.ts
+++ src/app/shared/components/home/notes/note-card/note-card.spec.ts
@@ -1,47 +1,53 @@
-import { inject } from '@angular/core/testing'
-import { TestComponentBuilder, ComponentFixture } from '@angular/compiler/testing'
-import { Component } from '@angular/core'
-import { NoteCard } from './index'
-import { By } from '@angular/platform-browser'
+import { TestBed, async } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { NoteCard } from './index';
+import { By } from '@angular/platform-browser';
 
 @Component( {
-    directives: [ NoteCard ],
-    template: '<note-card [note]="note" (checked)="onChecked($event)"></note-card>'
+  template: '<note-card [note]="note" (checked)="onChecked($event)"></note-card>'
 } )
 class TestComponent {
-    newNote: {
-        title: string,
-        content: string,
-        color: string
-    }
-    note = {
-        title: "test title",
-        content: "test content"
-    }
-    onChecked(note) {
-        this.newNote = note
-    }
+  newNote: {
+    title: string,
+    content: string,
+    color: string
+  };
+  note = {
+    title: 'test title',
+    content: 'test content'
+  };
+
+  onChecked( note ) {
+    this.newNote = note;
+  }
 }
 
-describe( 'NoteCard', ()=> {
-    let fixture: ComponentFixture<TestComponent>;
+describe( 'NoteCard', () => {
 
-    beforeEach( inject( [ TestComponentBuilder ], ( builder: TestComponentBuilder )=> {
-        fixture = builder.createSync( TestComponent )
-    } ) )
+  beforeEach( () => {
+    TestBed.configureTestingModule( {
+      declarations: [
+        TestComponent,
+        NoteCard
+      ],
+    } );
+  } );
 
-    it( "the component should have a title", ()=> {
-        fixture.detectChanges()
-        const el = fixture.debugElement.query( By.css( '.title' ) )
-        expect( el.nativeElement.textContent.trim() ).toBe( 'test title' )
-    } )
+  it( 'the component should have a title', async( () => {
+    let fixture = TestBed.createComponent( TestComponent );
+    fixture.detectChanges();
+    const el = fixture.debugElement.query( By.css( '.title' ) );
+    expect( el.nativeElement.textContent.trim() ).toBe( 'test title' );
+  } ) );
 
-    it( "the component will change when click the check", ()=> {
-        fixture.detectChanges()
-        const testComponent:TestComponent = fixture.componentInstance
-        const el = fixture.debugElement.query( By.css( '.icon' ) )
+  it( 'the component will change when click the check', async( () => {
+    let fixture = TestBed.createComponent( TestComponent );
+    fixture.detectChanges();
+    const testComponent: TestComponent = fixture.componentInstance;
+    const el = fixture.debugElement.query( By.css( '.icon' ) );
 
-        el.triggerEventHandler( 'click', null )
-        expect(testComponent.newNote.title).toBe('test title')
-    } )
-} )
+    el.triggerEventHandler( 'click', null );
+    expect( testComponent.newNote.title ).toBe( 'test title' );
+  } ) );
+
+} );
